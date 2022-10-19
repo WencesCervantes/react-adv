7510ce3b9f793bea4a12ed456eaf2eac7fbc4510
Wences Cervantes <wences.cervantes>
2022-10-19 12:38:43 -0500

    Control Props Implementado

--- src/02-source-component-patterns/hooks/useProduct.ts
+++ src/02-source-component-patterns/hooks/useProduct.ts
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Product, onChangeArgs } from "../interfaces/interfaces";
 
-export const useProduct = () => {
-  const [counter, setCounter] = useState(0);
+interface useProductArgs {
+  product: Product;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
+}
+
+export const useProduct = ({
+  onChange,
+  product,
+  value = 0,
+}: useProductArgs) => {
+  const [counter, setCounter] = useState(value);
+
+  const isControlled = useRef(!!onChange); //con esto vemos si el onChange esta siendo controlado, o sea si fue llamado
 
   const increaseBy = (value: number) => {
-    setCounter((prev) => Math.max(prev + value, 0)); //lo que esta linea hace es que no pueda ser un valor menor a 0
+    if (isControlled.current) {
+      return onChange!({ count: value, product });
+    }
+
+    const newValue = Math.max(counter + value, 0);
+
+    setCounter(newValue); //lo que esta linea hace es que no pueda ser un valor menor a 0
+
+    onChange && onChange({ count: newValue, product }); //esta codigo funciona como un if, si onchange no es nulo, entonces ejecuta la funcion, sino, no hagas nada.
   };
+
+  useEffect(() => {
+    //este es el que se encarga de que se refleje el contador en los productos del carrito
+    setCounter(value);
+  }, [value]);
 
   return {
     counter,
